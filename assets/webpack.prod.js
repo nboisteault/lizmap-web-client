0d8ba25de9415a0c47fbc809e9924356d4347a85
Nicolas Boisteault <nboisteault@3liz.com>
2019-06-14 10:07:36 +0200

    Mise en place webpack

---
+++ assets/webpack.prod.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		map: './src/map.js'
+	},
+	output: {
+		filename: '../../lizmap/www/js/[name].js',
+		chunkFilename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+};
