47449c5660aa9aba9e8fcededcacbb73b06e7126
Nicolas Boisteault <nboisteault@3liz.com>
2019-08-03 22:32:28 +0200

    Lien aide webpack + leaflet

--- assets/src/leafletMapElement.js
+++ assets/src/leafletMapElement.js
@@ -1,13 +1,7 @@
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet/dist/leaflet.js';
 
-// /* This code is needed to properly load the images in the Leaflet CSS */
-// delete L.Icon.Default.prototype._getIconUrl;
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
-//   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-// });
+// https://github.com/btpschroeder/leaflet-webpack
 
 export default class leafletMapElement extends HTMLElement {
     constructor() {
