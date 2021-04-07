aedd242bc7a662556eafe6f5fc2408a5bcedbe83
Lucie-Masson <masson-lucie@laposte.net>
2021-04-07 10:06:28 +0200

    [e2e] first tests for layertreeview

---
+++ tests/js/cypress/integration/layer_treeview_spec.js
@@ -0,0 +1,32 @@
+describe('Form edition', function() {
+    beforeEach(function(){
+        cy.visit('/index.php/view/map/?repository=testsrepository&project=layer_treeview')
+    })
+
+    it('click layer button', function(){
+        cy.get('#layer-layer_treeview > td:nth-child(1) > button:nth-child(2)').click()
+        cy.get('#layer-layer_treeview > td:nth-child(1) > button:nth-child(2)').should('not.have.class', 'checked')
+        cy.get('#group-group1 > td:nth-child(1) > button:nth-child(2)').should('not.have.class', 'checked')
+    })
+
+    it('click layer expander', function(){
+        cy.get('#layer-layer_treeview > td:nth-child(1) > a:nth-child(1)').click()
+        cy.get('#layer-layer_treeview').should('have.class','expanded')
+    })
+
+    it('click group informations', function(){
+        cy.get('#group-group1 > td:nth-child(1) > span:nth-child(3)').click()
+        cy.get('#group-group1').should('have.class','selected')
+    })
+
+    it('click close button', function(){
+        cy.get('#dock-close').click()
+        cy.get('li.switcher').should('not.have.class','active')
+    })
+
+    it('click group button', function(){
+        cy.get('#group-group1 > td:nth-child(1) > button:nth-child(2)').click()
+        cy.get('#group-group1 > td:nth-child(1) > button:nth-child(2)').should('not.have.class', 'checked')
+        cy.get('#layer-layer_treeview > td:nth-child(1) > button:nth-child(2)').should('not.have.class', 'checked')
+    })
+})
