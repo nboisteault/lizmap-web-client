4083a32b3d98a862a0dea44fa715524fb16c47f9
Lucie-Masson <masson-lucie@laposte.net>
2021-04-08 10:51:25 +0200

    [visual test] Cypress Image Snapshot installation

--- tests/js/cypress/support/commands.js
+++ tests/js/cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+//Adds for Cypress Image Snapshot
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
+addMatchImageSnapshotCommand();
