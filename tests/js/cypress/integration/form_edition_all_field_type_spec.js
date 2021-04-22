5548bd997115617f3feecc69f0306e3c28b7a430
Lucie-Masson <masson-lucie@laposte.net>
2021-04-22 09:40:32 +0200

    [e2e] form edition all fiel type and type relational value

--- tests/js/cypress/integration/form_edition_all_field_type_spec.js
+++ tests/js/cypress/integration/form_edition_all_field_type_spec.js
@@ -3,49 +3,54 @@ describe('Form edition', function() {
         cy.visit('/index.php/view/map/?repository=testsrepository&project=form_edition_all_field_type')
         cy.get('#button-edition').click() 
         cy.get('#edition-draw').click()
-        cy.get('#jforms_view_edition_boolean_notnull_for_checkbox').click()
+        //cy.get('#jforms_view_edition_boolean_notnull_for_checkbox').click()
     })
 
     it('expected error, string', function(){
         cy.get('#jforms_view_edition_integer_field').type('foo')
-        cy.get('#jforms_view_edition__submit_submit').click().should('be.visible', '#jforms_view_edition_errors > p')
+        cy.get('#jforms_view_edition__submit_submit').click()
+        cy.get('#jforms_view_edition_errors > p').should('be.visible')
     })
 
     it('expected error, value too big', function(){
         cy.get('#jforms_view_edition_integer_field').type('2147483648')
-        cy.get('#jforms_view_edition__submit_submit').click().should('be.visible', '#jforms_view_edition_errors > p')
+        cy.get('#jforms_view_edition__submit_submit').click()
+        cy.get('#jforms_view_edition_errors > p').should('be.visible')
     })
 
     it('expected error, negative value too big', function(){
         cy.get('#jforms_view_edition_integer_field').type('-2147483649')
-        cy.get('#jforms_view_edition__submit_submit').click().should('be.visible', '#jforms_view_edition_errors > p')
+        cy.get('#jforms_view_edition__submit_submit').click()
+        cy.get('#jforms_view_edition_errors > p').should('be.visible')
     })
 
     it('success, negative value', function(){
         cy.get('#jforms_view_edition_integer_field').type('-1')
-        cy.get('#jforms_view_edition__submit_submit').click().should('be.visible', '#lizmap-edition-message')
+        cy.get('#jforms_view_edition__submit_submit').click()
+        cy.get('#lizmap-edition-message').should('be.visible')
     })
 
     it('success,zero value', function(){
         cy.get('#jforms_view_edition_integer_field').type('0')
-        cy.get('#jforms_view_edition__submit_submit').click().should('be.visible', '#lizmap-edition-message')
+        cy.get('#jforms_view_edition__submit_submit').click()
+        cy.get('#lizmap-edition-message').should('be.visible')
     })
 
     it('success, positive value', function(){
         cy.get('#jforms_view_edition_integer_field').type('1')
-        cy.get('#jforms_view_edition__submit_submit').click().should('be.visible', '#lizmap-edition-message')
+        cy.get('#jforms_view_edition__submit_submit').click()
+        cy.get('#lizmap-edition-message').should('be.visible')
     })
 
+    it('boolean, boolean_notnull_for_checkbox check', function(){
+        cy.get('#jforms_view_edition__submit_submit').click().should('be.visible')
+        cy.get('#lizmap-edition-message')
+    })
 
-    /*
-    ###  Boolean
-
-        **Note from Etienne 14/01/21**, these 2 tests below are failing for now, and it seems expected for now :(
-
-        * [ ] `boolean_notnull_for_checkbox` field should be submitted with or without being checked
-    */
-    it('boolean, ', function(){
-
+    it('boolean, boolean_notnull_for_checkbox uncheck', function(){
+        cy.get('#jforms_view_edition_boolean_notnull_for_checkbox').click()
+        cy.get('#jforms_view_edition__submit_submit').click()
+        cy.get('#lizmap-edition-message').should('be.visible')
     })
 
     it('boolean, dropdown menu', function(){
